2feea7082217e935a0b68edf442ae53f7c0a3efe
Code Sandbox <hello@codesandbox.io>
2021-06-04 23:11:50 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.78f4ba5e71ca538af375f5f9ffbdda53.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2af68f9f0c40dc818273b87f09ee52a5",
+    "url": "https://mesala01.github.io/csb-7v03f/index.html"
+  },
+  {
+    "revision": "bf44d2f07520b54f7f33",
+    "url": "https://mesala01.github.io/csb-7v03f/static/js/2.3535461f.chunk.js"
+  },
+  {
+    "revision": "d28d439a834e8eb5aa4d",
+    "url": "https://mesala01.github.io/csb-7v03f/static/js/main.4d5ba623.chunk.js"
+  },
+  {
+    "revision": "37618615cd4424260902",
+    "url": "https://mesala01.github.io/csb-7v03f/static/js/runtime~main.32b7bb1f.js"
+  }
+]);
